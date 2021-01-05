57bbe948fb50193112911b1f31019d015786118f
Marlohn <marlohn.choinski@gmail.com>
2021-01-05 20:31:10 -0300

    Prepare application layer

---
+++ src/LogChallenge.Application/Interfaces/Generics/IGenericApplication.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogChallenge.Application.Interfaces.Generics
+{
+   public interface IGenericApplication<T> where T : class
+    {
+        Task Add(T Object);
+        Task Update(T Object);
+        Task Delete(T Object);
+        Task<T> GetEntityById(Guid Id);
+        Task<List<T>> List();
+    }
+}
---
+++ src/LogChallenge.Application/Interfaces/ILogApplication.cs
@@ -0,0 +1,16 @@
+﻿using LogChallenge.Application.Interfaces.Generics;
+using LogChallenge.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogChallenge.Application.Interfaces
+{
+    public interface ILogApplication : IGenericApplication<Log>
+    {
+        Task AddLog(Log log);
+        Task UpdateLog(Log log);
+    }
+}
