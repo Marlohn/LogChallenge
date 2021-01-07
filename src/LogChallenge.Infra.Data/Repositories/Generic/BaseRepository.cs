d5bf18113daaddeb4c2527192bd1e9d4dacea144
Marlohn <marlohn.choinski@gmail.com>
2021-01-07 19:48:07 -0300

    Infra repositories implementation

--- src/LogChallenge.Infra.Data/Repositories/Generic/BaseRepository.cs
+++ src/LogChallenge.Infra.Data/Repositories/Generic/BaseRepository.cs
@@ -1,12 +1,77 @@
-﻿using System;
+﻿using LogChallenge.Domain.Entities.Generic;
+using LogChallenge.Domain.Interfaces.Repositories.Generic;
+using LogChallenge.Infra.Data.Contexts;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace LogChallenge.Infra.Data.Repositories.Generic
 {
-    class BaseRepository
+    public class BaseRepository<T> : IBaseRepository<T> where T : BaseEntity
     {
+        protected readonly Context _Context;
+
+        public BaseRepository(Context Context) : base()
+        {
+            _Context = Context;
+        }
+
+        public Task Add(T entity)
+        {
+            throw new NotImplementedException();
+            /*
+            contexto.InitTransacao();
+            var id = contexto.Set<TEntidade>().Add(entidade).Entity.Id;
+            contexto.SendChanges();
+            return id;
+            */
+        }
+
+        public Task Delete(Guid id)
+        {
+            throw new NotImplementedException();
+            /*
+            var entidade = SelecionarPorId(id);
+            if (entidade != null)
+            {
+                contexto.InitTransacao();
+                contexto.Set<TEntidade>().Remove(entidade);
+                contexto.SendChanges();
+            }
+            */
+        }
+
+        public Task Delete(T entity)
+        {
+            throw new NotImplementedException();
+            /*
+            contexto.InitTransacao();
+            contexto.Set<TEntidade>().Remove(entidade);
+            contexto.SendChanges();
+            */
+        }
+
+        public Task<List<T>> List()
+        {
+            throw new NotImplementedException();
+            //return contexto.Set<TEntidade>().ToList();
+        }
+
+        public Task<T> SelectById(Guid id)
+        {            
+            throw new NotImplementedException();
+            //return contexto.Set<TEntidade>().Find(id);
+        }
+
+        public Task Update(T entity)
+        {
+            throw new NotImplementedException();
+            /*
+            contexto.InitTransacao();
+            contexto.Set<TEntidade>().Attach(entidade);
+            contexto.Entry(entidade).State = EntityState.Modified;
+            contexto.SendChanges();
+            */
+        }
     }
 }
--- src/LogChallenge.Infra.Data/Repositories/LogRepository.cs
+++ src/LogChallenge.Infra.Data/Repositories/LogRepository.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using LogChallenge.Domain.Entities;
+using LogChallenge.Domain.Interfaces.Repositories;
+using LogChallenge.Infra.Data.Contexts;
+using LogChallenge.Infra.Data.Repositories.Generic;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,25 @@ using System.Threading.Tasks;
 
 namespace LogChallenge.Infra.Data.Repositories
 {
-    class LogRepository
+    public class LogRepository : BaseRepository<Log>, ILogRepository
     {
+        protected readonly Context _Context;
+
+        public LogRepository(Context contexto) : base(contexto)
+        {
+            _Context = contexto;
+        }
+
+        public Task AddLog(Log log)
+        {
+            //Registra em alguma base de dados
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateLog(Log log)
+        {
+            //Registra em alguma base de dados
+            throw new NotImplementedException();
+        }
     }
 }
