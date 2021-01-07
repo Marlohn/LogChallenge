cf8cf1bb80b17b25943467bf8b4b2fec278cbbd4
Marlohn <marlohn.choinski@gmail.com>
2021-01-07 18:12:05 -0300

    BaseApplicationService coded

--- src/LogChallenge.Application/Services/Generic/BaseApplicationService.cs
+++ src/LogChallenge.Application/Services/Generic/BaseApplicationService.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using AutoMapper;
+using LogChallenge.Application.Dto.Generic;
+using LogChallenge.Application.Interfaces.Generic;
+using LogChallenge.Domain.Entities.Generic;
+using LogChallenge.Domain.Interfaces.Services.Generic;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +11,47 @@ using System.Threading.Tasks;
 
 namespace LogChallenge.Application.Services.Generic
 {
-    class BaseApplicationService
+
+    public class BaseApplicationService<T, TDto> : IBaseApplication<T, TDto> where T : BaseEntity where TDto : BaseEntityDto
     {
+        protected readonly IBaseService<T> servico;
+        protected readonly IMapper iMapper;
+
+        public BaseApplicationService(IMapper iMapper, IBaseService<T> servico) : base()
+        {
+            this.iMapper = iMapper;
+            this.servico = servico;
+        }
+
+        public async Task Add(TDto entity)
+        {
+            await servico.Add(iMapper.Map<T>(entity));
+        }
+
+        public async Task Delete(Guid id)
+        {
+            await servico.Delete(iMapper.Map<T>(id));
+        }
+
+        public async Task Delete(TDto entity)
+        {
+            await servico.Delete(iMapper.Map<T>(entity));
+        }
+
+        public async Task Update(TDto entity)
+        {
+            await servico.Update(iMapper.Map<T>(entity));
+        }
+
+        public async Task<List<TDto>> List()
+        {
+            return iMapper.Map<List<TDto>>(await servico.List());
+        }
+
+        public async Task<TDto> SelectById(Guid id)
+        {
+           return iMapper.Map<TDto>(await servico.SelectById(id));
+        }
+
     }
 }
