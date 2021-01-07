300040fb2604c2b12d34db76d27bb4b3392d91f5
Marlohn <marlohn.choinski@gmail.com>
2021-01-07 19:52:25 -0300

    Implementation of DependencyInjector

--- src/LogChallenge.Infra.IoC/DependencyInjector.cs
+++ src/LogChallenge.Infra.IoC/DependencyInjector.cs
@@ -1,12 +1,46 @@
-﻿using System;
+﻿using LogChallenge.Application.Interfaces;
+using LogChallenge.Application.Interfaces.Generic;
+using LogChallenge.Application.Services;
+using LogChallenge.Application.Services.Generic;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AutoMapper;
+using LogChallenge.Domain.Interfaces.Services.Generic;
+using LogChallenge.Domain.Services.Generic;
+using LogChallenge.Domain.Services;
+using LogChallenge.Domain.Interfaces.Services;
+using LogChallenge.Infra.Data.Contexts;
+using LogChallenge.Domain.Interfaces.Repositories.Generic;
+using LogChallenge.Infra.Data.Repositories.Generic;
+using LogChallenge.Infra.Data.Repositories;
+using LogChallenge.Domain.Interfaces.Repositories;
 
 namespace LogChallenge.Infra.IoC
 {
-    class DependencyInjector
+    public class DependencyInjector
     {
+        public static void Registry(IServiceCollection svcCollection)
+        {
+            //Aplicação
+            svcCollection.AddScoped(typeof(IBaseApplication<,>), typeof(BaseApplicationService<,>));
+            svcCollection.AddScoped<ILogApplication, LogApplicationService>();
+            svcCollection.AddAutoMapper(typeof(LogApplicationService));
+
+            //Domínio
+            svcCollection.AddScoped(typeof(IBaseService<>), typeof(BaseService<>));
+            svcCollection.AddScoped<ILogService, LogService>();
+            svcCollection.AddAutoMapper(typeof(LogService));
+
+            //Repositorio
+            svcCollection.AddScoped(typeof(IBaseRepository<>), typeof(BaseRepository<>));
+            svcCollection.AddScoped<ILogRepository, LogRepository>();
+
+            //Context
+            svcCollection.AddDbContext<Context>(ServiceLifetime.Scoped);
+        }
     }
 }
