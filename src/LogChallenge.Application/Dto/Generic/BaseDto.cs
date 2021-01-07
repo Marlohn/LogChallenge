4a0da1180b724e376a1587e9c2268f1eabadaeaa
Marlohn <marlohn.choinski@gmail.com>
2021-01-07 13:17:24 -0300

    Create DTO files

--- src/LogChallenge.Application/Dto/Generic/BaseDto.cs
+++ src/LogChallenge.Application/Dto/Generic/BaseDto.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 
 namespace LogChallenge.Application.Dto.Generic
 {
-    class BaseDto
+    public class BaseDto
     {
+        public Guid Id { get; set; }
     }
 }
--- src/LogChallenge.Application/Dto/LogDto.cs
+++ src/LogChallenge.Application/Dto/LogDto.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LogChallenge.Application.Dto.Generic;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,16 @@ using System.Threading.Tasks;
 
 namespace LogChallenge.Application.Dto
 {
-    class LogDto
+    public class LogDto : BaseDto
     {
+        public string Host { get; set; }
+        public string Identity { get; set; }
+        public string User { get; set; }
+        public DateTime DateTime { get; set; }
+        public string Request { get; set; }
+        public int StatusCode { get; set; }
+        public int? Size { get; set; }
+        public string Referer { get; set; }
+        public string UserAgent { get; set; }
     }
 }
