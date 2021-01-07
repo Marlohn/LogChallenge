d2bda4e55ae96b2c636ae7feb98405823002880a
Marlohn <marlohn.choinski@gmail.com>
2021-01-07 13:13:22 -0300

    Add context file

--- src/LogChallenge.Infra.Data/Contexts/Context.cs
+++ src/LogChallenge.Infra.Data/Contexts/Context.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using LogChallenge.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,23 @@ using System.Threading.Tasks;
 
 namespace LogChallenge.Infra.Data.Contexts
 {
-    class Context
+    class Context : DbContext
     {
+        public DbSet<Log> Calculadora { get; set; }
+        public IDbContextTransaction Transaction { get; private set; }
+
+        public Context(DbContextOptions<Context> options): base(options)      
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            //modelBuilder.ApplyConfiguration(new LogMap());
+        }
     }
 }
