8f83b35dfcf328e64d22ed9b7960e66774c7b61a
Marlohn <marlohn.choinski@gmail.com>
2021-01-11 11:55:41 -0300

    Entities MAP file

--- src/LogChallenge.Application/MapEntities.cs
+++ src/LogChallenge.Application/MapEntities.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using AutoMapper;
+using LogChallenge.Application.Dto;
+using LogChallenge.Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +9,12 @@ using System.Threading.Tasks;
 
 namespace LogChallenge.Application
 {
-    public class MapEntities
+    public class MapEntities : Profile
     {
-
+        public MapEntities()
+        {
+            CreateMap<Log, LogDto>();
+            CreateMap<LogDto, Log>();
+        }
     }
 }
