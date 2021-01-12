ef2bf0223dde93a166d2484bc2b18ab45267088d
Marlohn <marlohn.choinski@gmail.com>
2021-01-12 12:32:49 -0300

    Add REQUIRED itens in DTO

--- src/LogChallenge.Application/Dto/LogDto.cs
+++ src/LogChallenge.Application/Dto/LogDto.cs
@@ -1,18 +1,33 @@
 ﻿using LogChallenge.Application.Dto.Generic;
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace LogChallenge.Application.Dto
 {
     public class LogDto : BaseEntityDto
     {
+        [Required]
         public string Host { get; set; }
+
+        [Required]
         public string Identity { get; set; }
+
+        [Required]
         public string User { get; set; }
+
+        [Required]
         public DateTime DateTime { get; set; }
+
+        [Required]
         public string Request { get; set; }
+
+        [Required]
         public int StatusCode { get; set; }
+
         public int? Size { get; set; }
+
         public string Referer { get; set; }
+
         public string UserAgent { get; set; }
     }
 }
