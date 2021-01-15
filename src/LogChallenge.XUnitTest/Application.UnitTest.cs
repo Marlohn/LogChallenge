0cd7b50e96ba2b3a5641c7162a1f5dd98d7581f1
Marlohn <marlohn.choinski@gmail.com>
2021-01-15 14:32:18 -0300

    LogAdd added tests

--- src/LogChallenge.XUnitTest/Application.UnitTest.cs
+++ src/LogChallenge.XUnitTest/Application.UnitTest.cs
@@ -29,8 +29,10 @@ namespace LogChallenge.XUnitTest
             _logFake = new LogFake();
         }
 
+        //---- LOG ADD --------------------------------------------------------------------//
+
         [Fact]
-        public void LogAdd_ReturnGuidAndErrorCountZero()
+        public void LogAdd_ReturnLogWithNotificationsCountZero()
         {
             // Arrange
             var log = _logFake.GenerateFull();
@@ -41,8 +43,213 @@ namespace LogChallenge.XUnitTest
 
             // Assert
             _logRepository.Verify(a => a.Add(It.IsAny<Log>()), Times.Once);
-            Assert.IsType<Guid>(result);
+            Assert.True(result.Notifications.Count == 0);
         }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData("MORECHARACTERSTHANALLOWED")]
+        public void LogAdd_HostInvalidParameterReturnNotification(string Host)
+        {
+            // Arrange
+            var log = _logFake.GenerateFull();
+            log.Host = Host;
+            _logRepository.Setup(x => x.Add(log)).ReturnsAsync(log.Id);
+
+            // Act            
+            var result = _logApplicationService.LogAdd(_mapper.Map<LogDto>(log)).Result;
+
+            // Assert
+            _logRepository.Verify(a => a.Add(It.IsAny<Log>()), Times.Never);
+            Assert.True(result.Notifications.Count > 0);
+        }
+
+        [Theory]
+        [InlineData("MORECHARACTERSTHANALLOWEDMORECHARACTERSTHANALLOWEDMORECHARACTERSTHANALLOWED")]
+        public void LogAdd_IdentityInvalidParameterReturnNotification(string Identity)
+        {
+            // Arrange
+            var log = _logFake.GenerateFull();
+            log.Identity = Identity;
+            _logRepository.Setup(x => x.Add(log)).ReturnsAsync(log.Id);
+
+            // Act            
+            var result = _logApplicationService.LogAdd(_mapper.Map<LogDto>(log)).Result;
+
+            // Assert
+            _logRepository.Verify(a => a.Add(It.IsAny<Log>()), Times.Never);
+            Assert.True(result.Notifications.Count > 0);
+        }
+
+        [Theory]
+        [InlineData("MORECHARACTERSTHANALLOWEDMORECHARACTERSTHANALLOWEDMORECHARACTERSTHANALLOWED")]
+        public void LogAdd_UserInvalidParameterReturnNotification(string User)
+        {
+            // Arrange
+            var log = _logFake.GenerateFull();
+            log.User = User;
+            _logRepository.Setup(x => x.Add(log)).ReturnsAsync(log.Id);
+
+            // Act            
+            var result = _logApplicationService.LogAdd(_mapper.Map<LogDto>(log)).Result;
+
+            // Assert
+            _logRepository.Verify(a => a.Add(It.IsAny<Log>()), Times.Never);
+            Assert.True(result.Notifications.Count > 0);
+        }
+
+        [Theory]
+        [InlineData(null)]
+        public void LogAdd_DateTimeInvalidParameterReturnNotification(DateTime DateTime)
+        {
+            // Arrange
+            var log = _logFake.GenerateFull();
+            log.DateTime = DateTime;
+            _logRepository.Setup(x => x.Add(log)).ReturnsAsync(log.Id);
+
+            // Act            
+            var result = _logApplicationService.LogAdd(_mapper.Map<LogDto>(log)).Result;
+
+            // Assert
+            _logRepository.Verify(a => a.Add(It.IsAny<Log>()), Times.Never);
+            Assert.True(result.Notifications.Count > 0);
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData("MORECHARACTERSTHANALLOWEDMORECHARACTERSTHANALLOWEDMORECHARACTERSTHANALLOWEDMORECHARACTERSTHANALLOWEDMORECHARACTERSTHANALLOWEDMORECHARACTERSTHANALLOWEDMORECHARACTERSTHANALLOWEDMORECHARACTERSTHANALLOWEDMORECHARACTERSTHANALLOWEDMORECHARACTERSTHANALLOWEDMORECHARACTERSTHANALLOWED")]        
+        public void LogAdd_RequestInvalidParameterReturnNotification(string Request)
+        {
+            // Arrange
+            var log = _logFake.GenerateFull();
+            log.Request = Request;
+            _logRepository.Setup(x => x.Add(log)).ReturnsAsync(log.Id);
+
+            // Act            
+            var result = _logApplicationService.LogAdd(_mapper.Map<LogDto>(log)).Result;
+
+            // Assert
+            _logRepository.Verify(a => a.Add(It.IsAny<Log>()), Times.Never);
+            Assert.True(result.Notifications.Count > 0);
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData(1)]
+        [InlineData(99)]
+        [InlineData(1000)]
+        public void LogAdd_StatusCodeInvalidParameterReturnNotification(int StatusCode)
+        {
+            // Arrange
+            var log = _logFake.GenerateFull();
+            log.StatusCode = StatusCode;
+            _logRepository.Setup(x => x.Add(log)).ReturnsAsync(log.Id);
+
+            // Act            
+            var result = _logApplicationService.LogAdd(_mapper.Map<LogDto>(log)).Result;
+
+            // Assert
+            _logRepository.Verify(a => a.Add(It.IsAny<Log>()), Times.Never);
+            Assert.True(result.Notifications.Count > 0);
+        }
+
+        [Theory]
+        [InlineData(-1)]
+        public void LogAdd_SizeInvalidParameterReturnNotification(int Size)
+        {
+            // Arrange
+            var log = _logFake.GenerateFull();
+            log.Size = Size;
+            _logRepository.Setup(x => x.Add(log)).ReturnsAsync(log.Id);
+
+            // Act            
+            var result = _logApplicationService.LogAdd(_mapper.Map<LogDto>(log)).Result;
+
+            // Assert
+            _logRepository.Verify(a => a.Add(It.IsAny<Log>()), Times.Never);
+            Assert.True(result.Notifications.Count > 0);
+        }
+
+        [Theory]
+        [InlineData("MORECHARACTERSTHANALLOWEDMORECHARACTERSTHANALLOWEDMORECHARACTERSTHANALLOWEDMORECHARACTERSTHANALLOWEDMORECHARACTERSTHANALLOWEDMORECHARACTERSTHANALLOWEDMORECHARACTERSTHANALLOWEDMORECHARACTERSTHANALLOWEDMORECHARACTERSTHANALLOWEDMORECHARACTERSTHANALLOWEDMORECHARACTERSTHANALLOWED")]
+        public void LogAdd_RefererInvalidParameterReturnNotification(string Referer)
+        {
+            // Arrange
+            var log = _logFake.GenerateFull();
+            log.Referer = Referer;
+            _logRepository.Setup(x => x.Add(log)).ReturnsAsync(log.Id);
+
+            // Act            
+            var result = _logApplicationService.LogAdd(_mapper.Map<LogDto>(log)).Result;
+
+            // Assert
+            _logRepository.Verify(a => a.Add(It.IsAny<Log>()), Times.Never);
+            Assert.True(result.Notifications.Count > 0);
+        }
+
+        [Theory]
+        [InlineData("MORECHARACTERSTHANALLOWEDMORECHARACTERSTHANALLOWEDMORECHARACTERSTHANALLOWEDMORECHARACTERSTHANALLOWEDMORECHARACTERSTHANALLOWEDMORECHARACTERSTHANALLOWEDMORECHARACTERSTHANALLOWEDMORECHARACTERSTHANALLOWEDMORECHARACTERSTHANALLOWEDMORECHARACTERSTHANALLOWEDMORECHARACTERSTHANALLOWED")]
+        public void LogAdd_UserAgentInvalidParameterReturnNotification(string UserAgent)
+        {
+            // Arrange
+            var log = _logFake.GenerateFull();
+            log.UserAgent = UserAgent;
+            _logRepository.Setup(x => x.Add(log)).ReturnsAsync(log.Id);
+
+            // Act            
+            var result = _logApplicationService.LogAdd(_mapper.Map<LogDto>(log)).Result;
+
+            // Assert
+            _logRepository.Verify(a => a.Add(It.IsAny<Log>()), Times.Never);
+            Assert.True(result.Notifications.Count > 0);
+        }
+
+        //---- LOG UPDATE --------------------------------------------------------------------//
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData("MORECHARACTERSTHANALLOWED")]
+        public void LogUpdateHostInvalidParameterReturnNotification(string Host)
+        {
+            // Arrange
+            var log = _logFake.GenerateFull();
+            log.Host = Host;
+            _logRepository.Setup(x => x.LogUpdate(log)).ReturnsAsync(log.Id);
+
+            // Act            
+            var result = _logApplicationService.LogUpdate(_mapper.Map<LogDto>(log)).Result;
+
+            // Assert
+            _logRepository.Verify(a => a.Add(It.IsAny<Log>()), Times.Never);
+            Assert.True(result.Notifications.Count > 0);
+        }
+
+
+
+
+
+
+
+        //[Theory]
+        //[InlineData(null, null, null, null, null, null, null, null, null)]
+        //[InlineData("10.1.1.1", null, null, null, null, null, null, null, null)]
+        //[InlineData("MORETHEN25CHARACTERSMORETHEN25CHARACTERS", null, null, null, null, null, null, null, null)]
+        //public void LogAdd_ReturnLogWithNotifications(string Host, string Identity, string User, DateTime DateTime, string Request, int StatusCode, int Size, string Referer, string UserAgent)
+        //{
+        //    // Arrange
+        //    var log = new Log() { Host = Host, User = User, DateTime = DateTime, Request = Request, StatusCode = StatusCode, Size = Size, Referer = Referer, UserAgent = UserAgent };
+        //    _logRepository.Setup(x => x.Add(log)).ReturnsAsync(log.Id);
+
+        //    // Act            
+        //    var result = _logApplicationService.LogAdd(_mapper.Map<LogDto>(log)).Result;
+
+        //    // Assert
+        //    _logRepository.Verify(a => a.Add(It.IsAny<Log>()), Times.Never);
+        //    Assert.True(result.Notifications.Count > 0);
+        //}
 
 
 
